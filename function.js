af5fdc990661d872650d94bcd46218eb7aef6b01
Eduardo de Souza Ilha <eduardoilhasouza@gmail.com>
2022-10-03 00:16:05 -0300

    fix e tela inicial

--- function.js
+++ function.js
@@ -44,5 +44,25 @@ type_matriz.addEventListener('input', function(e){
     if (e.target.value == "identidade") {
         result2.textContent = "Você acertou"
     }
+    else if(e.target.value == "i"){
+        result2.textContent =  "Essa é a letra inicial" 
+    }
+    else{
+        result2.textContent =  "Digite" 
+    }
 
+  })
+  let result3 = document.getElementById('result-3');
+  let size_matriz = document.getElementById('size-matriz');
+  size_matriz.addEventListener('input', function(e){
+    if (e.target.value == 4){
+       result3.textContent = "Você acertou"
+    }
+  })
+  let result4 = document.getElementById('result-4');
+  let num_row = document.getElementById('num-row');
+  num_row.addEventListener('input', function(e){
+    if (e.target.value == 4){
+       result4.textContent = "Você acertou"
+    }
   })
